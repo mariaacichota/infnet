89b1673e81a465c5a0fabfc7320ebcf8f8c18bef
Maria Cichota <maria.cichota@al.infnet.edu.br>
2025-12-15 21:38:50 -0300

    add pastas

---
+++ microsservicos-devops/tree-services-architecture/catalog-service/src/test/java/com/infnet/catalog_service/CatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infnet.catalog_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
